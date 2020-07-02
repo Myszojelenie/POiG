b176ca23d97e3cbb3c37263d4a8fbfffc3eab681
ewa.natalia.lis <ewa.natalia.lis@gmail.com>
2020-07-02 10:58:49 +0200

    Dodaj pliki projektów.

---
+++ POiG_lista_TO_DO/WidokZadania.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace POiG_lista_TO_DO
+{
+    /// <summary>
+    /// Logika interakcji dla klasy WidokZadania.xaml
+    /// </summary>
+    public partial class WidokZadania : Window
+    {
+        public WidokZadania()
+        {
+            InitializeComponent();
+        }
+    }
+}
